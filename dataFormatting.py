117eda7c2b362c80cd48b0154b3f536cd83d4f8b
Evan-OR <tehEvster@gmail.com>
2023-05-30 18:34:13 +0100

    added and foramtted data

---
+++ dataFormatting.py
@@ -0,0 +1,9 @@
+with open ('data/alice.txt', "r", encoding='utf8') as f:
+    text = f.read()
+    chapters = text.split('CHAPTER ')[1:]
+    
+    for i in range(len(chapters)):
+        file_path = f'data/Chapter {i}.txt'
+        new_file = open(file_path, 'w', encoding="utf8")
+        new_file.write(chapters[i])
+        new_file.close()
