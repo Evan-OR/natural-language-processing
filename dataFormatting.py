ac2f66ca476f2f29e310b97f755f640832f06833
18EOR <42158152+18EOR@users.noreply.github.com>
2023-06-04 22:13:37 +0100

    reading and manipulating text data with spacy

--- dataFormatting.py
+++ dataFormatting.py
@@ -1,9 +1,17 @@
-with open ('data/alice.txt', "r", encoding='utf8') as f:
+import spacy
+
+with open ('data/alice.txt', 'r', encoding='utf8') as f:
     text = f.read()
     chapters = text.split('CHAPTER ')[1:]
     
-    for i in range(len(chapters)):
-        file_path = f'data/Chapter {i}.txt'
-        new_file = open(file_path, 'w', encoding="utf8")
-        new_file.write(chapters[i])
-        new_file.close()
+    # for i in range(len(chapters)):
+    #     file_path = f'data/Chapter {i}.txt'
+    #     new_file = open(file_path, 'w', encoding="utf8")
+    #     new_file.write(chapters[i])
+    #     new_file.close()
+
+nlp = spacy.load('en_core_web_sm')
+doc = nlp(chapters[0])
+sents = list(doc.sents)
+print(sents[1])
+
